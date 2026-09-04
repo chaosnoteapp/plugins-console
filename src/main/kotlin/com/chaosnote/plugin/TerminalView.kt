539b98ff5b2806adad39f8cdd81c22b74d8b5672
Edgar <edgar.vahitov@gmail.com>
2026-09-04 15:05:01 +0300

    base

--- src/main/kotlin/com/chaosnote/plugin/TerminalView.kt
+++ src/main/kotlin/com/chaosnote/plugin/TerminalView.kt
@@ -1,21 +1,14 @@
 package com.chaosnote.plugin
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.SwingPanel
 import com.chaosnote.api.block.BlockHandle
-import com.jediterm.pty.PtyProcessTtyConnector
 import com.jediterm.terminal.TerminalColor
 import com.jediterm.terminal.TextStyle
 import com.jediterm.terminal.ui.JediTermWidget
 import com.jediterm.terminal.ui.settings.DefaultSettingsProvider
-import com.pty4j.PtyProcessBuilder
-import java.nio.charset.Charset
 
 
 val settings = object : DefaultSettingsProvider() {
